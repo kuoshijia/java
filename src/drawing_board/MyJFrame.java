dbc667d417758763cc3319bdc8c1d95dffc28175
kuoshijia <emailjia@qq.com>
2018-11-30 18:03:10 +0800

    --update--

--- src/drawing_board/MyJFrame.java
+++ src/drawing_board/MyJFrame.java
@@ -6,29 +6,39 @@ import javax.swing.*;
 import java.awt.*;
 
 public class MyJFrame extends JFrame {
-    public JButton[] buttons;
-    public Container pane;
+    //按钮
+    public MyButtons buttons;
+    //画板
+    public MyPanel panel;
+
+    //构造函数
     public MyJFrame() {
-        super("这是一个画板");
-        this.setSize(800,600);
-        this.setBackground(Color.white);
-        this.setDefaultCloseOperation(MyJFrame.EXIT_ON_CLOSE);
-        pane = getContentPane();
-        initButtons();
+        //自身属性设置
+        this.setSelf();
+        //添加组件
+        this.addPanel();
+        this.addJButtons();
+        //开始显示画框本身
         this.setVisible(true);
     }
-
-    private void initButtons() {
-        String[] sButtons = {"Line","Circle","Rectangle","Triangle","Characters"};
-        this.buttons = new JButton[sButtons.length];
-        Container paneButtons = new Container();
-        paneButtons.setLayout(new GridLayout(0,1,100,10));
-        pane.add(BorderLayout.EAST,paneButtons);
-        for(int i=0; i<sButtons.length;i++)
-        {
-            this.buttons[i] = new JButton(sButtons[i]);
-            paneButtons.add(buttons[i]);
-        }
-        //return this.buttons;
+//自身设置
+    private void setSelf()
+    {
+        this.setTitle("这是一个画板");
+        this.setSize(800,600);
+        this.setBackground(Color.white);
+        //this.setLayout(new BorderLayout());
+        this.setDefaultCloseOperation(MyJFrame.EXIT_ON_CLOSE);
     }
+//添加画板
+    private void addPanel() {
+        panel = new MyPanel();
+        this.add(BorderLayout.WEST,panel);
+    }
+//添加按钮
+    private void addJButtons() {
+        buttons = new MyButtons();
+        this.add(BorderLayout.EAST,buttons);
+    }
+
 }
