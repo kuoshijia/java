1aa77af4cc9ef895a95a97c6af29d12e7cfa3a7a
kuoshijia <emailjia@qq.com>
2018-12-01 19:17:48 +0800

    --update--

--- src/drawing_board/MyButtons.java
+++ src/drawing_board/MyButtons.java
@@ -3,11 +3,13 @@ package drawing_board;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
+import java.util.HashMap;
 
 //按钮类
 public class MyButtons extends Container{
-    String[] sButtons = {"Line","Circle","Rectangle","Triangle","Characters"};
-    JButton[] buttons;
+    private String[] sButtons = {"Line","Circle","Rectangle","Triangle","Characters"};
+    private HashMap<String, ActionListener> actionsMap;
+    public JButton[] buttons;
     public MyButtons() {
         //设置自身布局
         this.setLayout(new GridLayout(0,1,100,10));
@@ -23,6 +25,6 @@ public class MyButtons extends Container{
 
     protected ActionListener bindActionListener(String sButton)
     {
-
+        actionsMap.put();
     }
 }
