4acfc070c0ff241394d41a7d9b8a371ba0025449
kuoshijia <emailjia@qq.com>
2018-12-03 00:30:20 +0800

    minidrawpad

--- src/drawing_board/MyButtons.java
+++ src/drawing_board/MyButtons.java
@@ -2,20 +2,23 @@ package drawing_board;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 
 //按钮类
 public class MyButtons extends Container{
-    private String[] sButtons = {"Line","Circle","Rectangle","Triangle","Characters"};
-    private HashMap<String, ActionListener> actionsMap;
+    private ArrayList<String, ActionListener> actionsMap;
     public JButton[] buttons;
     public MyButtons() {
         //设置自身布局
         this.setLayout(new GridLayout(0,1,100,10));
         //添加按钮
-        this.buttons = new JButton[sButtons.length];
-        for(int i=0; i<sButtons.length;i++)
+        this.setActionListenerMap();
+        this.buttons = new JButton[actionsMap.size()];
+        for(Iterator iterator = actionsMap.keySet().iterator())
         {
             this.buttons[i] = new JButton(sButtons[i]);
             this.add(buttons[i]);
@@ -23,8 +26,15 @@ public class MyButtons extends Container{
 
     }
 
-    protected ActionListener bindActionListener(String sButton)
+    protected ActionListener setActionListenerMap()
     {
-        actionsMap.put();
+        String[] sButtons = {"Line","Circle","Rectangle","Triangle","Characters"};
+        actionsMap = new HashMap<String, ActionListener>();
+        actionsMap.put("Line", new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
     }
 }
